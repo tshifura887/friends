31e740b9c80dc62c5a0203d47295f97379288f4d
tshifura887 <85707650+tshifura887@users.noreply.github.com>
2022-05-30 10:25:09 +0200

    Friends app

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :friends
   get 'home/index'
   root 'home#index'
   get 'home/about'
+  get '/friends/new', to: 'friends#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
